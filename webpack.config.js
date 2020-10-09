d32626d1bcdbf73342ff86c23cb22c74d231b68a
SilvioMollov <33893512+SilvioMollov@users.noreply.github.com>
2020-10-09 17:25:17 +0300

    styling the welcoming page

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require("path");
 
 module.exports = {
-  //   mode: "development",
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
   module: {
     rules: [
