0c3474fd451e358e47f9ef9782cdb7d16e5d005e
SilvioMollov <33893512+SilvioMollov@users.noreply.github.com>
2020-10-12 16:45:55 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import _ from "lodash";
-import './styles/style.scss'
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+import "./styles/style.scss";
 
 // function component() {
 //   const element = document.createElement("div");
