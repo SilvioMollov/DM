9305189adf6c9b87c7e64c70f14f91bbfc7077e8
SilvioMollov <33893512+SilvioMollov@users.noreply.github.com>
2020-10-19 15:48:17 +0300

    fixed meta loading

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import './styles/style.scss';
-import _ from 'lodash';
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
-import Logo from './logo1.svg';
-
-function injectImgSrc() {
-  const logoContainer = document
-    .querySelector('.logo-header')
-    .querySelector('img');
-
-  logoContainer.src = Logo;
-}
-
-injectImgSrc();
+import "./styles/style.scss";
+import _ from "lodash";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-var HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+const path = require("path");
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: "src/index.html",
     }),
-    new FaviconsWebpackPlugin('src/favicon.ico'),
-    new HtmlWebpackTagsPlugin({
-      metas: [
-        {
-          path: '.src/logo1.svg',
-          attributes: {
-            property: 'og:image',
-          },
-        },
-      ],
-    }),
+    new FaviconsWebpackPlugin("src/favicon.ico"),
   ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: './dist',
-    host: '0.0.0.0',
+    contentBase: "./dist",
+    host: "0.0.0.0",
     useLocalIp: true,
     port: 3000,
   },
@@ -42,26 +32,55 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader',
+          "sass-loader",
         ],
       },
       {
         test: /\.(eot|png|svg|ttf|woff|woff2|jpg|gif|ico)$/,
         use: {
-          loader: 'file-loader',
+          loader: "file-loader",
         },
       },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
+
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              attributes: {
+                list: [
+                  "...",
+                  {
+                    tag: "meta",
+                    attribute: "content",
+                    type: "src",
+                    /**
+                     * @docs https://github.com/webpack-contrib/html-loader#list
+                     */
+                    filter: (_tag, _attribute, attributes, _resourcePath) => {
+                      if (
+                        attributes.property === "og:image" ||
+                        attributes.property === "twitter:image"
+                      ) {
+                        return true;
+                      }
+                      return false;
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
       },
     ],
   },
